21ea022b9b0cc029f4661213a081d8356002262f
Diego Rodriguez <diegorodriguezv@gmail.com>
2019-05-25 17:07:10 -0500

    Finished all 4 plots

--- plot1.R
+++ plot1.R
@@ -1,17 +1,21 @@
 data_folder <- "data"
-file_name <-
-  paste(data_folder, "household_power_consumption.txt", sep = "/")
+zip_name <- paste(data_folder,
+                  "exdata_data_household_power_consumption.zip",
+                  sep = "/")
+file_name <- "household_power_consumption.txt"
 # Line numbers obtained by inspection in a text editor
 first_line <- 66637
 last_line <- 69517
 data_names <-
-  read.table(file_name,
-             header = TRUE,
-             sep = ";",
-             nrows = 1)
+  read.table(
+    unz(zip_name, file_name),
+    header = TRUE,
+    sep = ";",
+    nrows = 1
+  )
 power_data <-
   read.table(
-    file_name,
+    unz(zip_name, file_name),
     header = FALSE,
     sep = ";",
     skip = first_line,
@@ -22,31 +26,13 @@ power_data <-
 names(power_data) <- names(data_names)
 power_data$DateTime <-
   strptime(paste(power_data$Date, power_data$Time), "%d/%m/%Y %H:%M:%S")
-lc <- Sys.getlocale("LC_TIME")
-Sys.setlocale("LC_TIME", "C")
+
 # plot1
+png(filename = "plot1.png")
 hist(
   power_data$Global_active_power,
   main = "Global Active Power",
   xlab = "Global Active Power (kilowatts)",
   col = "red"
 )
-# plot2
-plot(
-  power_data$DateTime,
-  power_data$Global_active_power,
-  ylab = "Global Active Power (kilowatts)",
-  xlab = "",
-  type = "n"
-)
-lines(power_data$DateTime, power_data$Global_active_power)
-# plot3|
-plot(
-  power_data$DateTime,
-  power_data$Global_active_power,
-  ylab = "Global Active Power (kilowatts)",
-  xlab = "",
-  type = "n"
-)
-lines(power_data$DateTime, power_data$Global_active_power)
-Sys.setlocale("LC_TIME", lc)
+dev.off()
